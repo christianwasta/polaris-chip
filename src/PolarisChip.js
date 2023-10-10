f6f8327eb4c45da52a3cc986fcaa6cd793ed2151
wasta <ccw5294@psu.edu>
2023-10-10 13:07:23 -0400

    Polaris-card initial add

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -3,24 +3,46 @@ import { LitElement, html, css } from 'lit';
 export class PolarisChip extends LitElement {
   static get properties() {
     return {
-      title: { type: String },
+      name: { type: String },
+      link: { type: String },
+      active: {type: Boolean, reflect: true},
     };
   }
 
   static get styles() {
     return css`
       :host {
-        display: block;
+        display: inline-block;
+        margin: 0 16px 16px 0;
+      }
+      .link:focus,
+      .link:hover,
+      :host([active]) .link {
+        background-color: #e4e5e7;
+        border: 2px solid #e4e5e7;
+        border-radius: 2px;
+        color: #004fa9;
+        text-decoration: underline;
+      }
+
+      .link {
+        font-weight: bold;
+        padding: 8px 4px;
+        border: 2px solid #ccc;
+        color: #444;
+        font-size: 16px;
       }
     `;
   }
 
   constructor() {
     super();
-    this.title = 'My boilerplate';
+    this.name = '';
+    this.link = 'https://www.psu.edu/news/academics/';
   }
 
   render() {
-    return html`<span>${this.title}</span>`;
+    return html`<a class="link" href="${this.link}"><slot>${this.name}</slot></a>`;
   }
 }
+
